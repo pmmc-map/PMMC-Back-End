9928ed3c7baa8a6dce38105b46300a664213cd6f
William Castelli <billycastelli@gmail.com>
2019-10-10 11:58:42 -0700

    Skeleton REST structure

--- app.py
+++ app.py
@@ -10,11 +10,26 @@ db = SQLAlchemy(app)
 # This is just a test, we will need to separate the models into classes
 # so it isnt too messy
 class Location(db.Model):
+	# Primary key will auto increment
     lid = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(128))
     state = db.Column(db.String(128), nullable=True)
     country = db.Column(db.String(128), nullable=False)
     # Add lon/lat, String?
+
+# REST API will be in format like...
+@app.route('/api/locations'):
+def add_location():
+    # Maybe location data will be given in the POST
+    # Parse json POST data into location object
+    # Create connection to db, db.session.add(new location)
+
+@app.route('/api/responses'):
+def add_response():
+    # Similar to add_location
+
+# Note: other routes involve GETting the analytics about each
+# location. /api/responses/count? Not sure how this should be setup..
 
 @app.route('/')
 def hello_world():
