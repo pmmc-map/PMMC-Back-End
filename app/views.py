ce40221197d219257f53afb9ae0dee309de7db1e
William Castelli <billycastelli@gmail.com>
2019-10-14 16:23:49 -0700

    Cleaned up POST

--- app/views.py
+++ app/views.py
@@ -5,22 +5,26 @@ import requests
 
 # REST API will be in format like...
 @app.route('/api/locations', methods=['GET', 'POST'])
-def add_location():
+def locations():
     # Still need to account for nullable locations
+    # Probably a sequence of statements like "if 'city' in request.json..." etc
     # Some locations might not have associated city, state, county (in the ocean, in another country...)
     if request.method == "POST":
-        # The data is sent as json
         lat_data = request.json["lat"]
         long_data = request.json["long"]
         city_data = request.json["city"]
         state_data = request.json["state"]
         country_data = request.json["country"]
-        #visit_date = request.json["visit"]
+        # Commented out for debugging 
+        # visit_date = request.json["visit"]
+
+        # By default, the lid will auto-increment (when not specified in the constructor)
         location_db = Location(lat = lat_data, long = long_data, city = city_data,
                                 state = state_data, country = country_data)
         db.session.add(location_db)
         db.session.commit()
-        return jsonify(success=True)    
+        return jsonify(success=True)   
+
     if request.method == "GET":
         # This is like querying the database
         all_locations = []
@@ -32,14 +36,13 @@ def add_location():
     return "No request sent"
 
 @app.route('/api/responses')
-def add_response():
+def responses():
     # Similar to add_location
-    return "Test"
-    #pass
+    return "Responses endpoint"
 
 # Note: other routes involve GETting the analytics about each
 # location. /api/responses/count? Not sure how this should be setup..
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Working!'
