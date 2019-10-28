3328209ddaf8a8d0d3433e50d858aabf4a2aa6d0
William Castelli <billycastelli@gmail.com>
2019-10-27 22:19:31 -0700

    Cleaned up schemas

--- app/survey.py
+++ app/survey.py
@@ -1,13 +1,11 @@
 from app import db
 
 class Question(db.Model):
-	# Primary key will auto increment
     qid = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, nullable=False)
     response = db.relationship('Response', backref='question')
 
 class Response(db.Model):
-    # I have no idea what the fields will be
     rid = db.Column(db.Integer, primary_key=True)
     qid = db.Column(db.Integer, db.ForeignKey('question.qid'), nullable=False)    
     text = db.Column(db.String, nullable=False)
