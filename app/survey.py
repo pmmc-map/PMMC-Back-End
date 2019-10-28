56cd24d5a04f635f86b96c3415aebb9f21060ca6
William Castelli <billycastelli@gmail.com>
2019-10-28 11:14:37 -0700

    Added Options schema and relationships

--- app/survey.py
+++ app/survey.py
@@ -4,6 +4,12 @@ class Question(db.Model):
     qid = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, nullable=False)
     response = db.relationship('Response', backref='question')
+    option = db.relationship('Option', backref='question')
+
+class Option(db.Model):
+    oid = db.Column(db.Integer, primary_key=True)
+    qid = db.Column(db.Integer, db.ForeignKey('question.qid'), nullable=False)    
+    text = db.Column(db.String, nullable=False)
 
 class Response(db.Model):
     rid = db.Column(db.Integer, primary_key=True)
