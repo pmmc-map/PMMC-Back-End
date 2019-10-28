3cd16f9d304b65eb94870bdb68e4d2124b678ccd
Hibah Wasim <hwasim@uci.edu>
2019-10-28 14:41:36 -0700

    Updated endpoint for AnimalLocations

--- app/animalLocatorView.py
+++ app/animalLocatorView.py
@@ -3,8 +3,8 @@ from app.models import AnimalLocations
 from flask import jsonify, make_response, request, url_for
 import requests, datetime, urllib
 
-@app.route('api/animal_locations', methods=['Get', 'POST'])
-@cross_origin(supports_credentials=True)
+@app.route('api/animal_locations', methods=['GET', 'POST'])
+
 def animalLocations():
     if request.method == "POST":
         lat_data = request.json["lat"]
