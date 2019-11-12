284b623d9c0a1dd1d695f1586278941cba3cc5a8
William Castelli <billycastelli@gmail.com>
2019-11-11 18:42:40 -0800

    Add VisitorResponse class

--- app/survey.py
+++ app/survey.py
@@ -3,6 +3,7 @@ from app import db
 class Question(db.Model):
     qid = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
     response = db.relationship('Response', backref='question')
     option = db.relationship('Option', backref='question')
 
@@ -15,3 +16,7 @@ class Response(db.Model):
     rid = db.Column(db.Integer, primary_key=True)
     qid = db.Column(db.Integer, db.ForeignKey('question.qid'), nullable=False)    
     text = db.Column(db.String, nullable=False)
+
+class VisitorResponse(db.Model):
+    rid = db.Column(db.Integer, primary_key=True)
+    vr_timestamp = db.Column(db.DateTime)
