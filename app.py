d8d498dbbf231ddf95ba4cf9e39024bbfd68df4c
William Castelli <billycastelli@gmail.com>
2019-11-13 14:52:16 -0800

    Local build

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from flask_cors import CORS
 
 if __name__=="__main__":
     CORS(app, support_credentials=True)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)#, host='0.0.0.0', port=5000)
