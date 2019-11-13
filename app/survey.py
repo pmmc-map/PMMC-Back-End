be5859aef0bea7ef17d002cd3008bc02317ee499
William Castelli <billycastelli@gmail.com>
2019-11-13 13:15:28 -0800

    Update survey

--- app/survey.py
+++ app/survey.py
@@ -4,13 +4,14 @@ class Question(db.Model):
     qid = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, nullable=False)
     active = db.Column(db.Boolean, nullable=False)
-    response = db.relationship('Response', backref='question')
+    #response = db.relationship('Response', backref='question')
     option = db.relationship('Option', backref='question')
 
 class Option(db.Model):
     oid = db.Column(db.Integer, primary_key=True)
     qid = db.Column(db.Integer, db.ForeignKey('question.qid'), nullable=False)    
     text = db.Column(db.String, nullable=False)
+    vr = db.relationship('VisitorResponse', backref='option')
 
 class Response(db.Model):
     rid = db.Column(db.Integer, primary_key=True)
@@ -18,5 +19,5 @@ class Response(db.Model):
     text = db.Column(db.String, nullable=False)
 
 class VisitorResponse(db.Model):
-    rid = db.Column(db.Integer, primary_key=True)
-    vr_timestamp = db.Column(db.DateTime)
+    vr_timestamp = db.Column(db.DateTime, primary_key=True)
+    oid = db.Column(db.Integer, db.ForeignKey('option.oid'))
