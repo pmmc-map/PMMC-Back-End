4c6c14dab58085fb9f9dc840df2429e06a698c49
Hibah Wasim <hwasim@uci.edu>
2019-11-14 11:28:07 -0800

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\hibah_wasim\\PMMC-Back-End\\app\\test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-from app import views, models
+from app import views, models, animalLocatorView
 
 db.create_all()
 
