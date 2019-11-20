bca5d66a67ff81fa13be13acd6db117e8ac84e26
William Castelli <billycastelli@gmail.com>
2019-11-20 15:57:42 -0800

    Cors resource handling

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from app import app
 from flask_cors import CORS
 
 if __name__=="__main__":
-    CORS(app, support_credentials=True)
+    CORS(app, support_credentials=True, resources={r"*": {"origins": "*"}})
     app.run(debug=True)#, host='0.0.0.0', port=5000)
