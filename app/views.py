9cce57f40c244dfea70791356f86a9ee60a57b95
William Castelli <billycastelli@gmail.com>
2019-11-21 11:30:53 -0800

    Added params to counts endpoint

--- app/views.py
+++ app/views.py
@@ -138,12 +138,12 @@ def location_counts():
     total_visitors = Location.query.count()
     country_count = Location.query.with_entities(Location.country).distinct().count()
     state_count = Location.query.filter_by(country="USA").with_entities(Location.state).distinct().count()
-    # Confirm that these are equivalent
-    # states = set()
-    # for l in Location.query.all():
-    #     if l.country == "USA":
-    #         states.add(l.state)
-    # state_count = len(states)
+    if request.args.get("country") and request.args.get("state"):
+        selected_state = request.args.get("state")
+        selected_country = request.args.get("country")
+        selected_state_count = Location.query.filter_by(state=selected_state).with_entities(Location.state).distinct().count()
+        selected_country_count = Location.query.filter_by(country=selected_country).with_entities(Location.state).distinct().count()
+        return jsonify(success=True, total_visitors=total_visitors, unique_states = state_count, unqiue_countries=country_count, this_state_count = selected_state_count, this_country_count = selected_country_count)
     return jsonify(success=True, total_visitors=total_visitors, unique_states = state_count, unqiue_countries=country_count)
 
 # GETs location based on country name
