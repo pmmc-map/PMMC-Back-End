35fc37a6216b3e2e536e2ff323d4dce26a1a7208
Julia Lydick <julialydick15@gmail.com>
2019-11-21 11:22:58 -0800

    Updated toCsv to work with new schema

--- app/toCsv.py
+++ app/toCsv.py
@@ -2,19 +2,21 @@ import csv
 import pandas as pd
 from app import app, db
 from app.models import Location, CityImages
-from app.survey import Question, Response, Option
+from app.survey import Question, Option, VisitorResponse
 from flask_cors import CORS, cross_origin
 
 
 
-def sql_to_csv(tables=[Question, Response, Option], key='qid', name='mydump'):
-    combined = db.session.query(*tables)
-    for table in tables[1:]:
-        combined = combined.join(table)#, tables[0].qid == table.qid) USE IF NOT USING FOREIGN KEYS
-
-    df = pd.read_sql(combined.statement, db.session.bind)
-    df = df.loc[:,~df.columns.duplicated()]
-    del df[key] 
-    df.to_csv(name, index=False) 
-    return open(name + '.csv', 'w'), name 
-
+def sql_to_csv(tables=[Question, Option, VisitorResponse], name='mydump'):
+    if len(tables) == 1:
+    	df = pd.read_sql(db.session.query(tables[0]).statement, db.session.bind)
+    	df.to_csv(name + '.csv', index=False) 
+    else:
+    	combined = db.session.query(tables[0])
+    	combined = combined.join(tables[1])
+    	combined = combined.join(tables[2], tables[1].oid == tables[2].oid)
+    	df = pd.read_sql(combined.statement, db.session.bind)
+    	print(df)
+    	# df = df.loc[:,~df.columns.duplicated()]
+    	df.to_csv(name+ '.csv', index=False) 
+    return open(name + '.csv', 'w')
