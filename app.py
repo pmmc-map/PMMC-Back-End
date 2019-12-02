21e9e0dd12d41b08c7b0391a5f3d70edbeba2a13
Ubuntu <ubuntu@ip-172-31-15-24.us-west-1.compute.internal>
2019-12-02 00:16:29 +0000

    Changes made for deployment

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from flask_cors import CORS
 
 if __name__=="__main__":
     CORS(app, support_credentials=True, resources={r"*": {"origins": "*"}})
-    app.run(debug=True)#, host='0.0.0.0', port=5000)
+    app.run(debug=True, host="0.0.0.0", port=80)#, ssl_context='adhoc')#, ssl_context=('cert.pem', 'key.pem'))
+
