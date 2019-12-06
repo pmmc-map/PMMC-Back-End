8861108fb4766ee818a9721dff983b2460763171
Jas Delgado <jas@jasdelgado.com>
2019-12-06 14:53:48 -0800

    update animal information and new images, use csv instead of text file

--- animalsParser.py
+++ animalsParser.py
@@ -2,22 +2,22 @@ from app import app, db
 from app.models import AnimalLocations
 from flask import jsonify, make_response, request, url_for
 import requests, datetime, urllib
+import csv
 
 def addAnimals():
-    animal_file = open("AnimalLocations.txt", 'r')
-    for line in animal_file:
-        list = line.strip().split('*')
-        json = {}
-
+    animal_file = open("AnimalLocations.csv", 'r')
+    animal_reader = csv.reader(animal_file, delimiter=',', quotechar='"')
+    next(animal_reader)
+    for row in animal_reader:
         b64_image = None
-        image = None
+        image = ''
 
-        if(list[7] != ""):
-            file = open(list[7], "rb")
+        if(row[7] != ""):
+            file = open(row[7].replace('\n','').strip(), "rb")
             image = file.read()
-            
-        animal = AnimalLocations(animal_name=list[2], lat=list[5], long=list[6], location_name=list[0], placement_year=list[1], animal_type=list[3], animal_notes=list[4], animal_images=image)
+
+        animal = AnimalLocations(animal_name=row[0], lat=row[5], long=row[6], location_name=row[2], placement_year=row[3], animal_type=row[1], animal_notes=row[4], animal_images=image)
         db.session.add(animal)
         db.session.commit()
     animal_file.close()
-    
+
